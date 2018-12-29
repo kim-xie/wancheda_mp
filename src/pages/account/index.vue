3265ddba51724c267bbfe58afe53efeb64d31a0a
kim <1134771121@qq.com>
2018-12-29 23:20:48 +0800

    账号管理

--- src/pages/account/index.vue
+++ src/pages/account/index.vue
@@ -1,45 +1,251 @@
 <template>
   <div class="container">
     <div class="account">
-      <i-panel title="账号列表">
-        <i-cell-group>
-          <i-cell v-for="(item,index) in accountData" @tap="lookAccountDetail(item)" :key="index" :title="item.username" value="详细信息" is-link :url="'/pages/accountDetail/main?id='+item.id"></i-cell>
-        </i-cell-group>
-      </i-panel>
+      <div class="account_header">
+        <span class="search">
+          <input type="text" v-model="searchVal" @blur="goSearch" placeholder="按用户名或全名搜索">
+          <i v-if="searchVal === ''" class="iconfont icon-search"></i>
+          <i v-if="searchVal !== ''" class="iconfont icon-delete" style="color:red" @tap="clear"></i>
+        </span>
+        <span class="add_button float-right" @tap="handleAdd">新增</span>
+      </div>
+      <!-- iview 全局提示组件 -->
+      <i-message id="message"/>
+      <!-- 加载中组件 -->
+      <i-spin size="large" fix v-if="spinShow"></i-spin>
+
+      <div class="account_item" v-if="listData.length>0" v-for="(item,index) in listData" :key="index">
+        <div class="item_header">
+          <p class="item_name">
+            <span class="name">{{item.username}}</span>
+            <span class="code float-right">{{item.fullname}}</span>
+          </p>
+        </div>
+        <div class="item_detail">
+          <p class="detail">
+            <p class="">
+              <span class="label">所属门店:</span>
+              <span class="value">{{item.date.company.name}}</span>
+            </p>
+            <p class="">
+              <span class="label">所属角色:</span>
+              <span class="value">{{item.date.role.value}}</span>
+            </p>
+          </p>
+          <p class="detail">
+            <p class="">
+              <span class="label">手机号码:</span>
+              <span class="value">{{item.mobile}}</span>
+            </p>
+            <p class="">
+              <span class="label">状态:</span>
+              <span class="value disable" v-if="item.isDisable">禁用</span>
+              <span class="value undisable" v-if="!item.isDisable">启用</span>
+            </p>
+          </p>
+          <p class="detail">
+            <p class="">
+              <span class="label">备注:</span>
+              <span class="value">{{item.description}}</span>
+            </p>
+            <p class="">
+              <span class="label">创建时间:</span>
+              <span class="value">{{item.createTime}}</span>
+            </p>
+          </p>
+        </div>
+        <div class="item_footer">
+          <p class="item_edit">
+            <span class="button undisable" v-if="item.isDisable" @tap="handleStatus(item)">启用</span>
+            <span class="button disable" v-if="!item.isDisable" @tap="handleStatus(item)">禁用</span>
+            <span class="button edit" @tap="handleEdit(item)">编辑</span>
+            <span class="button delete" @tap="handleDelete(item.id)">删除</span>
+          </p>
+        </div>
+      </div>
+
+      <!-- 暂无数据 -->
+      <i-divider v-if="totalData===0 && searchVal !== ''" color="#2d8cf0" lineColor="#2d8cf0">抱歉，暂无数据</i-divider>
+
+      <!-- 页底加载 -->
+      <i-load-more v-if="totalData>pageSize" :tip="tipmessage" :loading="loading" />
+
+      <i-modal title="删除确认" :visible="modalVisible" :actions="actions" @tap="handleClick($event)">
+        <view>删除后数据将无法恢复哦</view>
+      </i-modal>
     </div>
   </div>
 </template>
 
 <script>
+  import globe from '../../utils/globe'
   import api from '../../api/api'
-  import bus from '../../utils/bus'
+  import * as utils from '../../assets/js/utils'
   export default {
     data () {
       return {
-        accountData: [],
-        totalData: 0
+        modalVisible: false,
+        spinShow: true,
+        tipmessage: '我也是有底线的',
+        loading: false,
+        listData: [],
+        totalData: 0,
+        pageNo: 1,
+        pageSize: 8,
+        form: {},
+        search: {
+          fullname: '',
+          username: ''
+        },
+        searchVal: '',
+        deleteId: '',
+        actions: [
+            {
+                name: '取消'
+            },
+            {
+                name: '删除',
+                color: '#ed3f14',
+            }
+        ]
       }
     },
     mounted() {
-      this.getAccountList()
+      this.spinShow = false
+    },
+    onLoad(){
+      this.listData = []
+      this.getList(this.pageNo, this.pageSize)
+    },
+    // 下拉刷新
+    onPullDownRefresh() {
+      console.log('下拉刷新')
+      console.log(this.pageNo)
+      if(this.pageNo > 1){
+        this.pageNo = this.pageNo-1
+        this.getList(this.pageNo, this.pageSize, function(){
+          wx.stopPullDownRefresh()
+        })
+      }
+    },
+    // 上拉加载，拉到底部触发
+    onReachBottom() {
+      // 到这底部在这里需要做什么事情
+      console.log('上拉加载')
+      const that = this
+      if(this.pageNo < this.totalData/this.pageSize){
+        this.loading = true
+        this.tipmessage = '玩命加载中'
+        this.pageNo = this.pageNo+1
+        this.getList(this.pageNo, this.pageSize, function(){
+          that.loading = false
+          that.tipmessage = '我也是有底线的'
+        })
+      }
     },
     methods: {
-      // 查看账号详情
-      lookAccountDetail(item){
-        console.log(item)
-        bus.$emit('getAccountDetail', item)
-      },
-      // 获取账号列表
-      getAccountList(pageNo, pageSize){
-        const params = {
-          pageNo,
-          pageSize
+      // 搜索
+      goSearch(){
+        console.log(this.searchVal)
+        const searchVal = this.searchVal
+        this.search.fullname = ''
+        this.search.username = ''
+        if(utils.isChinese(searchVal)){
+          this.search.fullname = searchVal
+        }else{
+          this.search.username = searchVal
         }
+        this.getList(this.pageNo, this.pageSize)
+      },
+      clear(){
+        this.searchVal = ''
+        this.search.fullname = ''
+        this.search.username = ''
+        this.getList(this.pageNo, this.pageSize)
+      },
+      // 获取列表数据
+      getList(pageNo, pageSize, callback){
+        const params = {
+          'search.fullname_like': this.search.fullname,
+          'search.username_like': this.search.username,
+          'page.pn': pageNo,
+          'page.size': pageSize
+        }
+        this.spinShow = true
         this.$http.get(api.account_list, params).then( res => {
-          console.log(res)
+          this.spinShow = false
           if(res.success){
-            this.accountData = res.data.page.content
+            this.listData = res.data.page.content
             this.totalData = res.data.page.totalElements
+            if(callback && typeof callback == 'function'){
+              callback()
+            }
+          }
+        })
+      },
+      // 新增维修项目
+      handleAdd(){
+        wx.navigateTo({
+          url: '/pages/accountDetail/main'
+        })
+      },
+      // 编辑维修项目
+      handleEdit(item){
+        this.$store.dispatch('saveEditItem', item).then(() => {
+          wx.navigateTo({
+            url: '/pages/accountDetail/main?id='+item.id
+          })
+        })
+      },
+      // 启用禁用
+      handleStatus(item){
+        const params = {
+          id: item.id,
+          isDisable: item.isDisable===false ? true : false
+        }
+        this.spinShow = true
+        this.$http.post(api.account_edit, params).then( res => {
+          console.log(res.success)
+          if(res.success){
+            globe.message(res.errorMsg, 'success')
+            this.getList(this.pageNo, this.pageSize)
+            this.spinShow = false
+          }else{
+            globe.message(res.errorMsg, 'warning')
+            this.spinShow = false
+          }
+        })
+      },
+      // 删除确认
+      handleClick(data){
+        const type = data.mp._relatedInfo.anchorTargetText
+        const that = this
+        if(type === '删除'){
+          that.deleteApi(that.deleteId,function(){
+            that.modalVisible = false
+          })
+        }else{
+          that.modalVisible = false
+        }
+      },
+      // 删除维修项目
+      handleDelete(id){
+        this.deleteId = id
+        this.modalVisible = true
+      },
+      deleteApi(id,callback){
+        this.spinShow = true
+        this.$http.delete(api.account_delete+"?id="+id,null).then( res => {
+          if(res.success){
+            globe.message(res.errorMsg,'success')
+            this.getList(this.pageNo, this.pageSize)
+            if(callback && typeof callback == 'function'){
+              callback()
+            }
+            this.spinShow = false
+          }else{
+            globe.message(res.errorMsg,'warning')
+            this.spinShow = false
           }
         })
       }
@@ -48,7 +254,78 @@
 </script>
 
 <style lang="scss" scoped>
-  .account{
-    width: 100%;
+.disable{
+  color: red!important;
+}
+.undisable{
+  color: green!important;
+}
+.account{
+  width: 100%;
+  .account_header{
+    width: 90%;
+    padding: 6px;
+    background-color: #f2f4fb;
+    height: 140rpx;
+    margin: 0 auto;
+    .add_button{
+      padding: 3px 10px;
+      border: 1px solid $--color-info;
+      border-radius: 4px;
+      margin-top:4px;
+    }
+    .search{
+      display: block;
+      position: relative;
+      input{
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 3rpx 80rpx 6rpx 12rpx;
+      }
+      .iconfont{
+        position: absolute;
+        right: 12rpx;
+        top: 8rpx;
+        font-size: 22px;
+
+      }
+    }
   }
+  .account_item{
+    width: 90%;
+    background-color: #f2f4fb;
+    margin: 20px auto;
+    border: 1px solid #f2f4fb;
+    border-radius: 6px;
+    padding: 6px;
+    .item_header{
+      border-bottom: 1px solid $--color-info;
+      padding: 0 0 4px 0;
+      .code{
+        color: $--color-info;
+      }
+    }
+    .item_detail{
+      padding: 6px 0;
+      .label{
+        display: inline-block;
+        width: 200rpx;
+        color: $--color-text-regular;
+      }
+    }
+    .item_footer{
+      border-top: 1px solid $--color-info;
+      text-align: center;
+      padding-top: 6px;
+      .button{
+        display: inline-block;
+        padding: 2px 10px;
+        border: 1px solid $--color-info;
+        border-radius: 4px;
+        margin: 0 8px;
+        color: $--color-text-regular;
+      }
+    }
+  }
+}
 </style>
--- src/pages/accountDetail/index.vue
+++ src/pages/accountDetail/index.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="container">
     <div class="accountDetail">
+      <!-- iview 全局提示组件 -->
+      <i-message id="message"/>
+      <!-- 加载中组件 -->
+      <i-spin size="large" fix v-if="spinShow"></i-spin>
       <p class="input_wrap">
         <span class="input_label">账号名</span>
         <input v-model="form.username"/>
       </p>
       <p class="input_wrap">
         <span class="input_label">真实姓名</span>
-        <input v-model="form.realname"/>
+        <input v-model="form.fullname"/>
+      </p>
+      <p class="input_wrap" v-if="id === ''">
+        <span class="input_label">初始密码</span>
+        <input type="password" v-model="password"/>
       </p>
       <p class="input_wrap">
         <span class="input_label">联系电话</span>
-        <input v-model="form.phone"/>
+        <input v-model="form.mobile"/>
       </p>
       <p class="input_wrap">
         <span class="input_label">邮箱</span>
@@ -19,11 +27,19 @@
       </p>
       <p class="input_wrap">
         <span class="input_label">所属公司</span>
-        <input v-model="form.company"/>
+        <picker @change="bindPickerChange($event, 'form', 'company')" :range="companys">
+          <input v-model="company" readonly/>
+        </picker>
       </p>
       <p class="input_wrap">
         <span class="input_label">角色</span>
-        <input v-model="form.role"/>
+        <picker @change="bindPickerChange($event, 'form', 'role')" :range="roles">
+          <input v-model="role" readonly/>
+        </picker>
+      </p>
+      <p class="input_wrap">
+        <span class="input_label">备注</span>
+        <input v-model="form.description"/>
       </p>
       <button class="save" @click="handleSave">保 存</button>
     </div>
@@ -32,27 +48,177 @@
 
 <script>
   import globe from '../../utils/globe'
-  import bus from '../../utils/bus'
+  import api from '../../api/api'
+  import CryptoJS from 'crypto-js/core'
+  import MD5 from 'crypto-js/md5'
+  import { setTimeout } from 'timers'
+  import { mapGetters } from 'vuex'
   export default {
     data () {
       return {
+        id: '',
         form: {},
-        companyId: ''
+        password: '',
+        companys: [],
+        companyIds: [],
+        company: '',
+        roles: [],
+        roleIds: [],
+        role: '',
+        spinShow: true,
       }
     },
-    mounted(data) {
+    onLoad() {
+      this.company = ''
+      this.role = ''
+      this.form = {}
+      this.spinShow = false
+      this.id = ''
       console.log(globe.getCurrentPageUrlArgs())
-      const urlParams = globe.getCurrentPageUrlArgs()
-      this.companyId = urlParams.split('=')[1]
-      this.getCompanyDetail(this.companyId)
-      bus.$on('getAccountDetail', (item) => {
-        console.log(item)
-      })
+      if(globe.getCurrentPageUrlArgs()){
+        const urlParams = globe.getCurrentPageUrlArgs()
+        this.id = urlParams.split('=')[1]
+        if(this.id){
+          this.getDetail()
+        }
+      }
+      // 角色
+      this.getLookupByCodeAndPicker('user_role', 'role')
+      // 获取公司列表
+      this.getCompanyList('company')
     },
     methods: {
-      // 获取公司详情
-      getCompanyDetail(id){
-        console.log(id)
+      // 获取详情
+      getDetail(){
+        let item = this.$store.getters.editItem
+        this.company = item.date.company.name
+        this.role = item.date.role.value
+        this.currentStatus = item.isDisabled
+        this.form = item
+      },
+      // 普通选择器
+      bindPickerChange(data, formName, type){
+        const index = data.mp.detail.value
+        // 显示的值
+        this[type] = this[type+'s'][index]
+        // 对应的id
+        this[formName][type] = this[type+'Ids'][index]
+      },
+      // 获取数据字典并且弹出选择框
+      getLookupByCodeAndPicker(code, type, successBack){
+        const _this = this
+        this.getLookupByCode(code, 1, 1000, function(data, total){
+          let dataArry = []
+          let idArry = []
+          for(let i=0; i<data.length; i++){
+            dataArry.push(data[i].value)
+            idArry.push(data[i].id)
+          }
+
+          _this[type+'s'] = dataArry
+          _this[type+'Ids'] = idArry
+
+          if(successBack && typeof successBack == 'function'){
+            successBack(dataArry, idArry)
+          }
+        })
+      },
+      // 获取公司列表
+      getCompanyList(type, successBack){
+        const params = {
+          'search.isDeleted_eq': false,
+          pageNo: 1,
+          pageSize: 1000
+        }
+        const _this = this
+        this.spinShow = true
+        this.$http.get(api.company_list, params).then( res => {
+          if(res.success){
+            const data = res.data.page.content
+            console.log(data)
+            let dataArry = []
+            let idArry = []
+            for(let i=0; i<data.length; i++){
+              dataArry.push(data[i].name)
+              idArry.push(data[i].id)
+            }
+            _this[type+'s'] = dataArry
+            _this[type+'Ids'] = idArry
+            this.spinShow = false
+            if(successBack && typeof successBack == 'function'){
+              successBack(dataArry, idArry)
+            }
+          }else{
+            this.spinShow = false
+          }
+        })
+      },
+      // 根据数据字典code获取数据字典
+      getLookupByCode(code, pageNo, pageSize, callback){
+        const params = {
+          pageNo,
+          pageSize
+        }
+        this.spinShow = true
+        this.$http.get(api.getLookupByCode + code, params).then( res => {
+          if(res.success){
+            this.spinShow = false
+            if(callback && typeof callback == 'function'){
+              callback(res.data.page.content, res.data.page.totalElements)
+            }
+          }
+          this.spinShow = false
+        })
+      },
+      // 保存
+      handleSave(callback){
+        let url = ''
+        if(this.id){
+          url = api.account_edit
+          delete this.form.date
+          delete this.form.password
+          delete this.form.createTime
+          delete this.form.updateTime
+          delete this.form.isDeleted
+        }else{
+          url = api.account_add
+          const hashPwd = CryptoJS.MD5(this.password).toString()
+          this.form.password = hashPwd
+        }
+        if(!this.form.username){
+          globe.message('账号名称不能为空', 'warning')
+          return false
+        }else if(!this.form.fullname){
+          globe.message('真实姓名不能为空', 'warning')
+          return false
+        }else if(!this.form.password){
+          globe.message('初始密码不能为空', 'warning')
+          return false
+        }else if(!this.form.company){
+          globe.message('所属公司不能为空', 'warning')
+          return false
+        }else if(!this.form.role){
+          globe.message('所属角色不能为空', 'warning')
+          return false
+        }else if(!this.form.mobile){
+          globe.message('手机不能为空', 'warning')
+          return false
+        }
+        this.spinShow = true
+        this.$http.post(url, this.form).then( res => {
+          if(res.success){
+            globe.message(res.errorMsg, 'success')
+            setTimeout(function(){
+              this.spinShow = false
+              wx.navigateTo({
+                url: '/pages/account/main'
+              })
+            },1000)
+          }else{
+            globe.message(res.errorMsg, 'warning')
+            this.spinShow = false
+          }
+        })
       }
     }
   }
@@ -74,6 +240,14 @@
         width: 100px;
         font-size: 18px;
         color: $--color-info;
+      }
+      .radio_group{
+        width: calc(100% - 106px);
+        height: 100%;
+        line-height: 70rpx;
+        .radio{
+          float: left;
+        }
       }
       input{
         width: calc(100% - 106px);
