9e2643055c82deec630d3e12dfd5481c0eda42b3
kim <1134771121@qq.com>
2019-01-19 18:41:25 +0800

    初始版本

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,49 +1,49 @@
 export const userInfo = (state) => {
-  console.log(state.userInfo)
+  //console.log(state.userInfo)
   return state.userInfo
 }
 
 export const inpartCount = (state) => {
-  console.log(state.inpartCount)
+  //console.log(state.inpartCount)
   return state.inpartCount
 }
 
 export const outpartCount = (state) => {
-  console.log(state.outpartCount)
+  //console.log(state.outpartCount)
   return state.outpartCount
 }
 
 export const repairItemCount = (state) => {
-  console.log(state.repairItemCount)
+  //console.log(state.repairItemCount)
   return state.repairItemCount
 }
 
 export const editItem = (state) => {
-  console.log(state.editItem)
+  //console.log(state.editItem)
   return state.editItem
 }
 
 export const client = (state) => {
-  console.log('client',state.client)
+  //console.log('client',state.client)
   return state.client
 }
 
 export const repairItems = (state) => {
-  console.log('repairItems', state.repairItems)
+  //console.log('repairItems', state.repairItems)
   return state.repairItems
 }
 
 export const repairItemIds = (state) => {
-  console.log('repairItemIds', state.repairItemIds)
+  //console.log('repairItemIds', state.repairItemIds)
   return state.repairItemIds.join(',')
 }
 
 export const inventoryItems = (state) => {
-  console.log('inventoryItems', state.inventoryItems)
+  //console.log('inventoryItems', state.inventoryItems)
   return state.inventoryItems
 }
 
 export const inventoryItemIds = (state) => {
-  console.log('inventoryItemIds', state.inventoryItemIds)
+  //console.log('inventoryItemIds', state.inventoryItemIds)
   return state.inventoryItemIds.join(',')
 }
